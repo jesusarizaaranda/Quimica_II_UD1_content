0498127c990066edc61da5cc5684d1c3a4a2c212
ciencies <104925502+jesusarizaaranda@users.noreply.github.com>
2025-10-07 14:00:06 +0200

    Add files via upload

---
+++ data/slide33.js
@@ -0,0 +1,16 @@
+(function(){var loadHandler=window['sl_{EA1056C7-74CE-4B7C-AF33-9F2E6CEF2187}'];loadHandler&&loadHandler(32, '<div id="spr0_3970a1ae"><div id="spr1_3970a1ae" class="kern slide"><img id="img2_3970a1ae" src="data/img1.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_3970a1ae" class="kern slide"><div id="spr3_3970a1ae" style="left:27px;top:98px;"><img id="img0_3970a1ae" src="data/img59.png" width="650" height="395" alt="Distribueix espècies carregades elèctricament en funció de la seva relació massa/càrrega. L’energia absorbida produeix la ruptura d’enllaços en la molècula analitzada i que provoca la fragmentació de la molècula en diferents trossos que es poden detectar.\
+\
+Passes.\
+L’analit s’introdueix en una càmera de buit.\
+La mostra es vaporitza.\
+Es bombardeja amb electrons d’alta energia que provoca la ionització de la molècula formant un catió-radical-molecular (M+). També es produeixen ruptures d’enllaços i la fragmentació molecular.\
+\
+𝑀+  𝑒 −  ⟶  𝑀 +  +2 𝑒 − \
+\
+\
+Els fragments carregats s’acceleren mitjançant un camp elèctric i es fan passar per un selector de velocitats per aconseguir un feix de partícules amb la mateixa velocitat (v).\
+El feix de partícules s’introdueix en un camp magnètic (B) que provoca la desviació de la trajectòria de la partícula en funció de la seva relació  (m/q) segons l’expressió:\
+\
+𝑹= 𝒗·𝒎 𝑩·𝒒 \
+\
+"/></div><div id="spr4_3970a1ae" style="left:56.323px;top:21.427px;"><div style="width:0px;"><span id="txt0_3970a1ae" data-width="414.667969" style="left:7.2px;top:-4.744px;">7. Espectrometria de masses</span></div></div><div id="spr5_3970a1ae" style="left:12.25px;top:26.477px;"><img id="img1_3970a1ae" src="data/img60.png" width="667" height="414" alt="" style="left:-0.235px;top:-0.048px;"/></div><div id="spr6_3970a1ae" style="left:36.25px;top:485.457px;"><div style="width:0px;"><span id="txt1_3970a1ae" data-width="195.169922" style="left:7.2px;top:1.323px;">Espectrometria de masses</span></div></div><div id="svg0_3970a1ae" style="left:19.25px;top:441.051px;"><svg width="239" height="40" viewBox="-5 -5 239 40"><path fill="none" stroke="#000000" stroke-opacity="0" stroke-width="5" d="M0,0 h228.021 v29.081 h-228.021 Z"/></svg></div><div id="spr7_3970a1ae" style="left:24.25px;top:446.051px;"><div style="width:0px;"><span id="txt2_3970a1ae" data-width="200.100586" style="left:7.2px;top:1.323px;">L’espectròmetre de masses</span></div></div></div></div>', '{"s":[]}');})();
